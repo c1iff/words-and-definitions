a34ea1ceda2d0863d3e0a3bea742378677d11b0d
Chris Clifford <cclifford82@gmail.com>
2016-12-02 10:17:01 -0800

    add routing to view a word and its definitions

--- app.rb
+++ app.rb
@@ -6,5 +6,18 @@ require('./lib/definition')
 
 
 get('/') do
+  @test_word1 = Word.new(:word => 'ruby')
+  @test_word1.save()
+  @test_word2 = Word.new(:word => 'object')
+  @test_word2.save()
+  @test_word3 = Word.new(:word => 'method')
+  @test_word3.save()
+  @words = Word.all()
   erb(:index)
+end
+
+get('/word/:id') do
+  @current_word = Word.find(params.fetch('id').to_i)
+  @definitions = @current_word.definitions()
+  erb(:word)
 end
