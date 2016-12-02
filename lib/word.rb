4df3b82809d87dbd821d3fa43fe44fc1a875219c
Chris Clifford <cclifford82@gmail.com>
2016-12-01 20:45:36 -0800

    add class method to store word objects

--- lib/word.rb
+++ lib/word.rb
@@ -1,6 +1,22 @@
 class Word
+
+  @@words = []
   attr_reader(:word)
+
   define_method(:initialize) do |attributes|
     @word = attributes.fetch(:word)
   end
+
+  define_singleton_method(:id) do
+    @@words
+  end
+
+  define_singleton_method(:clear) do
+    @@words = []
+  end
+  
+  define_method(:save) do
+    @@words.push(self)
+  end
+
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,11 +4,20 @@ require('word')
 describe(Word) do
 
   before() do
+    Word.clear()
     @test_word = Word.new(:word => 'variable')
+    @test_word.save()
   end
+
   describe('#word') do
-    it('returns the word that the word object stores') do
+    it('returns the word that the word object contains') do
       expect(@test_word.word()).to(eq('variable'))
+    end
+  end
+
+  describe('.id') do
+    it('returns the @@words class variable containing all saved words') do
+      expect(Word.id()).to(eq([@test_word]))
     end
   end
 end
