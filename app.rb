d389e591e6c550ec8e84315fd7d5b9320b7e5be4
Chris Clifford <cclifford82@gmail.com>
2016-12-02 11:29:24 -0800

    add routing to add a word and sort

--- app.rb
+++ app.rb
@@ -6,16 +6,6 @@ require('./lib/definition')
 
 
 get('/') do
-  @test_word1 = Word.new(:word => 'ruby')
-  @test_word1.save()
-
-  test_definition = Definition.new(:definition => 'not consistent or having a fixed pattern; liable to change.', :part_of_speech => 'adjective')
-  @test_word1.add_definition(test_definition)
-
-  @test_word2 = Word.new(:word => 'object')
-  @test_word2.save()
-  @test_word3 = Word.new(:word => 'method')
-  @test_word3.save()
   @words = Word.all()
   erb(:index)
 end
@@ -24,6 +14,22 @@ get('/word/:id') do
   @current_word = Word.find(params.fetch('id').to_i)
   @definitions = @current_word.definitions()
   erb(:word)
+end
+
+get('/new/word') do
+  erb(:new_word)
+end
+
+post('/add/word') do
+  @new_word = params.fetch('word')
+  Word.new(:word => @new_word).save()
+  erb(:success)
+end
+
+get('/sort/words') do
+  Word.sort_words_a_to_z()
+  @words = Word.all()
+  erb(:index)
 end
 
 get('/definition/new/:id') do
